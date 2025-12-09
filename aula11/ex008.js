54657bb74de90a64376723dcd562873eabcb3f82
Lucas Ferreira <lucasferreirasouza22@gmail.com>
2025-12-08 21:56:13 -0300

    aula12

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log("Exercício 08 - Aula 11");
+var velocidade = 60;
+
+
+if (velocidade > 60) { //condição simples
+    console.log(`Velocidade do veículo: ${velocidade} km/h. MULTADO! Você excedeu o limite de velocidade permitida que é de 60 km/h`);
+}
+console.log(`Velocidade do veículo: ${velocidade} km/h. Você está dentro do limite de velocidade permitida.`);
