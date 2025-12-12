1d490557d2fbad46465cb42e81cc7e6226fb0c09
Lucas Ferreira <lucasferreirasouza22@gmail.com>
2025-12-12 15:44:12 -0300

    for in and indexOf

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,7 +1,29 @@
-let vetor = [5,6,7]
+let vetor = [4,5,6]
 
 console.log(`Nosso vetor é: ${vetor}`)
 
+
 for (var i=0;i<vetor.length;i++){
-    console.log(`Elemento ${i+1} = Valor: ${vetor[i]} | Indíce: ${i}`)
+    console.log(`Elemento ${i} = Valor: ${vetor[i]} | Indíce: ${i}`)
 }
+
+vetor[3] = 7
+vetor.push(8)
+
+console.log(`Adicionando novos elementos no vetor: ${vetor}`)
+
+console.log(`Length é: ${vetor.length}`)
+
+console.log(`Sort é: ${vetor.sort()}`)
+
+for (let i in vetor){
+    console.log(`Elemento ${i} = Valor: ${vetor[i]} | Indíce: ${i}`)
+}
+for (var i=0;i<vetor.length;i++){
+    if(vetor.indexOf(i)==(-1)){
+        console.log(`Não encontra valor vetor.indexOf(2): ${vetor.indexOf(i)}`)
+    }else if(vetor.indexOf(i)!=(-1)){
+        console.log(`Encontra valor vetor.indexOf(4): ${vetor.indexOf(i)}`)
+    }
+}
+
